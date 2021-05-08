0dc442ccc2ce65c7950cfc97b2ee049f82705080
Bhavika Patel <bhavij28@gmail.com>
2021-05-07 21:25:48 -0400

    Updated employeetable to show employeedata

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -1,51 +1,52 @@
-import React from 'react';
-import API from '../../utils/API';
-import { Item, ItemMeta, Table } from 'semantic-ui-react';
-
-
+import React from "react";
+import API from "../../utils/API";
+import { Table, Header, Image } from "semantic-ui-react";
 
 class EmployeeTable extends React.Component {
-
     state = {
-
         results: [],
         sortOrder: "",
-        search: ""
-    }
+        search: "",
+    };
 
-    // When the component mounts, get a list of all available Employee 
+    // When the component mounts, get a list of all available Employee
     componentDidMount() {
-        API.getEmployeeList()
-            .then(res => this.setState({ results: res.data.results }))
-        console.log("Employee Data ==>", this.state.results)
+        API.getEmployeeList().then((res) =>
+            this.setState({ results: res.data.results }))
+            .catch(err => console.log(err))
+        console.log("Employee Data ==>", this.state.results);
     }
 
-    handleInputChange = event => {
+    handleInputChange = (event) => {
         this.setState({ search: event.target.value });
     };
 
-
     render() {
         return (
-
-            this.state.results.map(item =>
-
-                <Table celled>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>{item.picture.Image}</Table.HeaderCell>
-                            <Table.HeaderCell>{item.name.first}</Table.HeaderCell>
-                            <Table.HeaderCell>{item.name.last}</Table.HeaderCell>
-                            <Table.HeaderCell>{item.phone}</Table.HeaderCell>
-                            <Table.HeaderCell>{item.email}</Table.HeaderCell>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Image</Table.HeaderCell>
+                        <Table.HeaderCell>FirstName</Table.HeaderCell>
+                        <Table.HeaderCell>LastName</Table.HeaderCell>
+                        <Table.HeaderCell>Phone</Table.HeaderCell>
+                        <Table.HeaderCell>Email</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                {this.state.results.map((items) => (
+                    <Table.Body>
+                        <Table.Row key={items.uuid}> 
+                            <Table.Cell><Image src={items.picture.thumbnail} rounded size='mini' /></Table.Cell>
+                            <Table.Cell>{items.name.first}</Table.Cell>
+                            <Table.Cell>{items.name.last}</Table.Cell>
+                            <Table.Cell>{items.phone}</Table.Cell>
+                            <Table.Cell>{items.email}</Table.Cell>
                         </Table.Row>
-                    </Table.Header>
-           
-                </Table>
-            ));
-            
-        }
+                    </Table.Body>
+                ))}
+            </Table>
+        );
     }
+}
 
-export default EmployeeTable
-
+export default EmployeeTable;
--- src/utils/API.js
+++ src/utils/API.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export default {
     getEmployeeList: function () {
-        return axios.get("https://randomuser.me/api/?results=50");
+        return axios.get("https://randomuser.me/api/?results=50&seed=abc");
     }
 };
